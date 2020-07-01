422546cc490ee4ccee6fdc5873cc605976134005
Dnniz <dannuzp@gmail.com>
2020-07-01 18:24:01 -0500

    actualizacion de metodo async

--- src/async/challenge.js
+++ src/async/challenge.js
@@ -3,13 +3,17 @@ const fetchData = require("../utils/fetchData");
 let API = "https://rickandmortyapi.com/api/character/";
 
 const getData = async (url_api) => {
-  const data = await fetchData(url_api);
-  const character = await fetchData(`${API}${data.results[0].id}`);
-  const origin = await fetchData(`${character.origin.url}`);
+  try {
+    const data = await fetchData(url_api);
+    const character = await fetchData(`${API}${data.results[0].id}`);
+    const origin = await fetchData(`${character.origin.url}`);
 
-  console.log("[*]Total personajes:", data.info.count);
-  console.log("[*]Nombre personaje:", character.name);
-  console.log("[*]Nombre dimensión:", origin.dimension);
+    console.log("[*]Total personajes:", data.info.count);
+    console.log("[*]Nombre personaje:", character.name);
+    console.log("[*]Nombre dimensión:", origin.dimension);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 console.log("AFTER");
